8d0dfbd093d321a62704d5fd41ef6aaf6ac0b692
Seyedmohammad <hosseiniseyedmohammad3@gmail.com>
2023-01-03 18:55:11 +0330

    write menu header

--- menu.h
+++ menu.h
@@ -1,4 +1,23 @@
 #ifndef MENU_H
 #define MENU_H
+#include<iostream>
+using namespace std;
+
+enum zcommand{
+    _help,
+    _add_user,
+    _enter,
+    _quit,
+    _setting,
+    _jadval_score,
+    _guest,
+    _bad_command
+};
+
+void welcome_message();
+void help();
+void settings();
+zcommand getcommand();
+
 
 #endif // MENU_H
