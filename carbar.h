cafd3b6b9f2e392fb1ad9afa45e393ed8edbe6af
Seyedmohammad <hosseiniseyedmohammad3@gmail.com>
2023-01-03 19:01:21 +0330

    write carbar header

--- carbar.h
+++ carbar.h
@@ -1,4 +1,16 @@
 #ifndef CARBAR_H
 #define CARBAR_H
+#include "login.h"
+#include <map>
+class z_user{
+    map<string /*user name*/,login> tb_user;
+public:
+    z_user();
+    void add_user();
+    void sign_in();
+    void guest();
+    void jadval();
+    ~z_user();
+};
 
 #endif // CARBAR_H
